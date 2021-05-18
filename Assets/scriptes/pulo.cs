82403985c04a4df758b522fc02638281554fb9ef
ThiagoPSs <psthigo@gmail.com>
2021-05-18 17:30:17 -0300

    atualização

---
+++ Assets/scriptes/pulo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pulo : MonoBehaviour
+{
+    public Vector3 positionChange;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+
+            transform.position += positionChange;
+        }
+    }
+}
